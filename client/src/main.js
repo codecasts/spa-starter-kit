5068198a3c98411cf30cb9a50eb38de9db715ca0
Fabio Vedovelli <vedovelli@gmail.com>
2016-11-07 18:01:25 +0100

    jQuery and Bootstrap js includes

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require('./includes')
 import Vue from 'vue'
 import App from './App'
 
