c7fd5a495df16fdaa09d8bb2d58b041ae9f1d6de
Fabio Vedovelli <vedovelli@gmail.com>
2016-11-07 17:54:33 +0100

    Some comments

--- client/src/main.js
+++ client/src/main.js
@@ -2,13 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
+/**
+* This is the Vuex store and it is
+* avaible to all your components
+*/
 import store from './store'
+
+/**
+* This is the Router
+*/
 import router from './router'
 
 /* eslint-disable no-new */
 new Vue({
-  store,
-  router,
+  store, // injects the Store into all components
+  router, // make Router work with the application
   el: '#app',
   render: h => h(App)
 })
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+/**
+* Routes are always stored close to
+* the modules they help navigate to
+*/
 import dashboard from '../modules/dashboard/routes'
 import categories from '../modules/categories/routes'
 
@@ -10,7 +14,7 @@ const routes = [].concat(dashboard, categories)
 
 const router = new Router({
   routes,
-  mode: 'history',
+  mode: 'history', // do not use /#/.
 })
 
 export default router
