2156e256223c5648c2110e9c6050caa891f6a866
Fabio Vedovelli <vedovelli@gmail.com>
2016-11-08 17:59:47 +0100

    Placeholder for JWT protected routes

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -4,5 +4,8 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => ['cors', 'api']], function () {
     Route::post('login', ['uses' => 'LoginController@login']);
+    Route::group(['middleware' => 'jwt.auth'], function () {
+        // JWT protected routes here
+    });
 });
 
