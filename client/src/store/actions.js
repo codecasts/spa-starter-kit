275853e05b865156940935330704d1af32b50bc0
Fabio Vedovelli <vedovelli@gmail.com>
2016-11-08 19:07:45 +0100

    Some comments

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,17 +1,33 @@
 // https://vuex.vuejs.org/en/actions.html
 
 import * as TYPES from './types'
-import { set as localStorageSetItem } from '../utils/local'
+import { localStorageSetItem } from '../utils/local'
 
 export default {
   setToken: ({ commit }, token) => {
+    /**
+    * Sets the token to the local storage
+    * for auto-login purpose
+    */
     localStorageSetItem('token', { token })
+
+    /**
+    * Commit the mutations
+    */
     commit(TYPES.MAIN_SET_TOKEN, {
       token,
     })
   },
   setUser: ({ commit }, user) => {
+    /**
+    * Sets the user to the local storage
+    * for auto-login purpose
+    */
     localStorageSetItem('user', { user })
+
+    /**
+    * Commit the mutations
+    */
     commit(TYPES.MAIN_SET_USER, {
       user,
     })
