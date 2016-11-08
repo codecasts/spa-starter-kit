9ef4581de53b2abab754afb4eb128979a0e78033
Fabio Vedovelli <vedovelli@gmail.com>
2016-11-08 18:00:02 +0100

    Login returning a valid JWT

--- webservice/app/Http/Controllers/LoginController.php
+++ webservice/app/Http/Controllers/LoginController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use JWTAuth;
+use JWTException;
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        return response()->json($request->all(), 200);
+        // grab credentials from the request
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        // all good so return the token
+        return response()->json(compact('token'));
     }
 }
