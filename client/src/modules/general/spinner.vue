2a675237c18b1d4046a605d0aee4c473ea7439a0
Fabio Vedovelli <vedovelli@gmail.com>
2016-11-25 08:28:35 -0200

    Created a spinner component to be used globally

---
+++ client/src/modules/general/spinner.vue
@@ -0,0 +1,23 @@
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    computed: {
+      ...mapState(['fetching']),
+    },
+  }
+</script>
+
+<template>
+  <div class="container">
+    <i class="fa fa-fw fa-lg fa-circle-o-notch fa-spin" v-show="fetching"></i>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    display: inline-block;
+    width: 40px;
+  }
+</style>
