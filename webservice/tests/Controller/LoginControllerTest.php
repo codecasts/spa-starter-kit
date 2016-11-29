15c02886e7fcbde884b89816272c0b8ea3489584
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-11-29 14:22:40 -0200

    login test

--- webservice/tests/Controller/LoginControllerTest.php
+++ webservice/tests/Controller/LoginControllerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -15,12 +17,38 @@ class LoginControllerTest extends ApiTestCase
     {
         $this->json('POST', '/api/login', [
             'email' => 'hello@example.com',
-            'password' => 'dummypassaord',
+            'password' => 'dummypassword',
         ]);
 
         $this->assertResponseStatus(401);
         $this->seeJsonStructure([
             'messages' => [[]],
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function can_get_an_authenticated_token()
+    {
+        $email = 'hello@example.com';
+        $password = 'dummypassword';
+
+        $this->create(User::class, [
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->json('POST', '/api/login', [
+            'email' => $email,
+            'password' => $password,
+        ]);
+
+        $this->assertResponseOk();
+        $this->seeJsonStructure([
+            'token', 'user' => [
+                'id', 'name', 'email',
+            ],
         ]);
     }
 }
