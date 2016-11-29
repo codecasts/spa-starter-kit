fc02584053368f9b861d8ae674f717d8570a8d1d
Fabio Vedovelli <vedovelli@gmail.com>
2016-11-29 20:36:41 -0200

    Cover image included

--- client/src/app/dashboard/main.vue
+++ client/src/app/dashboard/main.vue
@@ -7,8 +7,15 @@
 
 <template>
   <div>
-      <blockquote>
-        A highly opinionated Single Page Application starter kit built on top of Vue.js and Laravel.
-      </blockquote>
+      <div class="text-center">
+        <img class="cover" src="http://vedovelli.com.br/spas.png" alt="Codecasts Single Page Application starter kit">
+        <h4>A highly opinionated Single Page Application starter kit built on top of Vue.js and Laravel.</h4>
+      </div>
   </div>
 </template>
+
+<style scoped>
+  .cover {
+    max-width: 800px;
+  }
+</style>
