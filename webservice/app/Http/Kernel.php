f386ec64d530931195d33c25370528e8599aa0ad
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-11-29 16:02:00 -0200

    fix

--- webservice/app/Http/Kernel.php
+++ webservice/app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Barryvdh\Cors\HandleCors:class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
