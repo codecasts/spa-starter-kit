2406cda4668c35d266cc7c957612568c566ca9d6
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-11-28 23:52:11 -0200

    added factory helper for tests

---
+++ webservice/tests/Support/Factory.php
@@ -0,0 +1,59 @@
+<?php
+
+trait Factory
+{
+    /**
+     * Number of times to run.
+     *
+     * @var int
+     */
+    protected $times = 1;
+
+    /**
+     * Create resource data.
+     *
+     * @param  string $resource
+     * @param  array  $data
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model
+     */
+    protected function create($resource, array $data = [])
+    {
+        if ($this->times > 1) {
+            return factory($resource, $this->times)->create($data);
+        }
+
+        return factory($resource)->create($data);
+    }
+
+    /**
+     * Make resource data.
+     *
+     * @param  string $resource
+     * @param  array  $data
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model
+     */
+    protected function make($resource, array $data = [])
+    {
+        if ($this->times > 1) {
+            return factory($resource, $this->times)->make($data);
+        }
+
+        return factory($resource)->make($data);
+    }
+
+    /**
+     * Set the number of times to run.
+     *
+     * @param  int $times
+     *
+     * @return self
+     */
+    protected function times($times)
+    {
+        $this->times = $times;
+
+        return $this;
+    }
+}
