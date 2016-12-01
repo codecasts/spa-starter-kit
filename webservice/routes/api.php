86f7ecf529196c64be450182aa10361979c7ca50
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-12-01 17:42:20 -0200

    using resource routes on the categories controller

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -1,17 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::group([
+    'middleware' => ['cors', 'api'],
+], function () {
+    Route::post('/login', 'LoginController@login');
 
-Route::group(['middleware' => ['cors', 'api']], function () {
-    Route::post('login', ['uses' => 'LoginController@login']);
-    Route::group(['middleware' => 'jwt.auth'], function () {
-        Route::group(['prefix' => 'categories'], function () {
-            Route::get('', ['uses' => 'CategoriesController@all']);
-            Route::get('{id}/get', ['uses' => 'CategoriesController@get']);
-            Route::post('create', ['uses' => 'CategoriesController@create']);
-            Route::put('{id}/update', ['uses' => 'CategoriesController@update']);
-            Route::delete('{id}/remove', ['uses' => 'CategoriesController@remove']);
-        });
+    Route::group([
+        'middleware' => 'jwt.auth',
+    ], function () {
+        Route::resource('/categories', 'CategoriesController', [
+            'except' => ['create', 'edit'],
+        ]);
     });
 });
+
+// Route::group(['middleware' => ['cors', 'api']], function () {
+//     Route::post('login', ['uses' => 'LoginController@login']);
+//     Route::group(['middleware' => 'jwt.auth'], function () {
+//         Route::group(['prefix' => 'categories'], function () {
+//             Route::get('', ['uses' => 'CategoriesController@all']);
+//             Route::get('{id}/get', ['uses' => 'CategoriesController@get']);
+//             Route::post('create', ['uses' => 'CategoriesController@create']);
+//             Route::put('{id}/update', ['uses' => 'CategoriesController@update']);
+//             Route::delete('{id}/remove', ['uses' => 'CategoriesController@remove']);
+//         });
+//     });
+// });
 
