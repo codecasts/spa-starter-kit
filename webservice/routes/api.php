adba59e2c9c292f52c903c7719e34cbe4eced035
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-12-01 19:00:40 -0200

    update categories urls on the client

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -13,17 +13,3 @@ Route::group([
         ]);
     });
 });
-
-// Route::group(['middleware' => ['cors', 'api']], function () {
-//     Route::post('login', ['uses' => 'LoginController@login']);
-//     Route::group(['middleware' => 'jwt.auth'], function () {
-//         Route::group(['prefix' => 'categories'], function () {
-//             Route::get('', ['uses' => 'CategoriesController@all']);
-//             Route::get('{id}/get', ['uses' => 'CategoriesController@get']);
-//             Route::post('create', ['uses' => 'CategoriesController@create']);
-//             Route::put('{id}/update', ['uses' => 'CategoriesController@update']);
-//             Route::delete('{id}/remove', ['uses' => 'CategoriesController@remove']);
-//         });
-//     });
-// });
-
