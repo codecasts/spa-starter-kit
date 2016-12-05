d4c906ff11a23b63cef28d23505a9bec1e1ae842
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-12-05 11:24:54 -0200

    new auth routes
    singin and singout

---
+++ client/src/app/auth/components/forms/singup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="well">
+    <h3>TODO</h3>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- client/src/app/auth/main.vue
+++ client/src/app/auth/main.vue
@@ -1,75 +1,27 @@
-
 <script>
-  import { mapActions } from 'vuex'
-
-  export default {
-    /**
-    * Component's local state
-    */
-    data() {
-      return {
-        email: 'happy.developer@vuejsisawesome.com',
-        password: '123456',
-      }
-    },
-    methods: {
-      /**
-      * Map the actions from Vuex to this component.
-      * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
-      */
-      ...mapActions(['setToken', 'setUser', 'setMessage']),
-
-      /**
-      * Handle form's submit event
-      */
-      submit() {
-        const payload = { email: this.email, password: this.password }
-        this.$http.post('auth/issue', payload).then((response) => {
-          this.handleResponse(response)
-        })
-      },
-
-      /**
-      * Handle success response from AJAX call
-      */
-      handleResponse(response) {
-        this.setToken(response.data.token) // this is a Vuex action
-        this.setUser(response.data.user) // this is a Vuex action
-        this.setMessage({ type: 'error', message: [] }) // this is a Vuex action
-        this.$router.push({ name: 'dashboard.index' })
-      },
-
-      /**
-      * Reset component's local state
-      */
-      reset() {
-        this.email = ''
-        this.password = ''
-      },
-    },
-  }
+export default {
+}
 </script>
 
 <template>
   <div class="container">
-    <h3 class="text-center">Codecasts - SPA - Starter Kit</h3>
-    <form class="well" @submit.prevent="submit">
-      <div class="form-group">
-        <label for="email" class="control-label">E-mail</label>
-        <input type="email" class="form-control" id="email" v-model="email">
+    <header class="page-header">
+      <h1>CODECASTS - SPA - Starter Kit</h1>
+    </header>
+    <div class="row">
+      <div class="col-md-4">
+        <ul class="nav nav-pills nav-stacked">
+          <router-link tag="li" :to="{ name: 'auth.singin' }">
+            <a>Entrar</a>
+          </router-link>
+          <router-link tag="li" :to="{ name: 'auth.singup' }">
+            <a>Cadastro</a>
+          </router-link>
+        </ul>
       </div>
-      <div class="form-group">
-        <label for="password" class="control-label">Password</label>
-        <input type="password" class="form-control" id="password" v-model="password">
+      <div class="col-md-8">
+        <router-view></router-view>
       </div>
-      <button class="btn btn-primary btn-block" type="submit">Login</button>
-    </form>
+    </div>
   </div>
 </template>
-
-<style scoped>
-  .container {
-    width: 400px;
-    margin: 100px auto;
-  }
-</style>
