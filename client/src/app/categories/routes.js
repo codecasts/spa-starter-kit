9ce569253c87e1aa37054c015311c9d8f3f73e32
Anthony Bouch <tony@58bits.com>
2016-12-06 17:09:12 +0700

    Reverted to code-splitting with group name

--- client/src/app/categories/routes.js
+++ client/src/app/categories/routes.js
@@ -2,25 +2,25 @@
  * Components are lazy-loaded
  * http://router.vuejs.org/en/advanced/lazy-loading.html
  *
- * An alternative syntax, to group all components into a single
- * Webpack 'chunk', or file is as follows:
+ * const Categories = (resolve) => {
+ *   require.ensure(['./main'], () => {
+ *     resolve(require('./main')) // eslint-disable-line global-require
+ *  })
+ * }
  *
- * const Categories = r => require.ensure([], () => r(require('./main.vue')), 'categories-bundle')
- * const Form = r => require.ensure([], () => r(require('./form.vue')), 'categories-bundle')
+ * const Form = (resolve) => {
+ *   require.ensure(['./form'], () => {
+ *     resolve(require('./form')) // eslint-disable-line global-require
+ *  })
+ * }
  *
+ * An alternative syntax used below, to group all components into a single
+ * Webpack 'chunk' and JS file.
  */
 
-const Categories = (resolve) => {
-    require.ensure(['./main'], () => {
-      resolve(require('./main')) // eslint-disable-line global-require
-  })
-}
+const Categories = r => require.ensure([], () => r(require('./main.vue')), 'categories-bundle')
+const Form = r => require.ensure([], () => r(require('./form.vue')), 'categories-bundle')
 
-const Form = (resolve) => {
-    require.ensure(['./form'], () => {
-      resolve(require('./form')) // eslint-disable-line global-require
-  })
-}
 
 const meta = {
   requiresAuth: true,
