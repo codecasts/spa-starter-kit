c1c64b62c34dd0dae972f1fa9ec2d6b395c2fa21
Anthony Bouch <tony@58bits.com>
2016-12-06 16:39:39 +0700

    Updated with commented examples

--- client/src/app/categories/routes.js
+++ client/src/app/categories/routes.js
@@ -1,11 +1,26 @@
 /**
- * Group components for code splitting in Web pack
- *
+ * Components are lazy-loaded
  * http://router.vuejs.org/en/advanced/lazy-loading.html
+ *
+ * An alternative syntax, to group all components into a single
+ * Webpack 'chunk', or file is as follows:
+ *
+ * const Categories = r => require.ensure([], () => r(require('./main.vue')), 'categories-bundle')
+ * const Form = r => require.ensure([], () => r(require('./form.vue')), 'categories-bundle')
+ *
  */
 
-const Categories = r => require.ensure([], () => r(require('./main.vue')), 'categories-bundle')
-const Form = r => require.ensure([], () => r(require('./form.vue')), 'categories-bundle')
+const Categories = (resolve) => {
+    require.ensure(['./main'], () => {
+      resolve(require('./main')) // eslint-disable-line global-require
+  })
+}
+
+const Form = (resolve) => {
+    require.ensure(['./form'], () => {
+      resolve(require('./form')) // eslint-disable-line global-require
+  })
+}
 
 const meta = {
   requiresAuth: true,
