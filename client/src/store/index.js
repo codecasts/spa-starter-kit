01a48a16da33f3be6e84c4411fc3d55888a8db61
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-12-13 16:53:40 -0200

    add getters
    shouldShowNavigation and isAuthPage

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,6 +5,7 @@ import mutations from './mutations'
 import actions from './actions'
 import modules from './modules'
 import plugins from './plugins'
+import * as getters from './getters'
 
 Vue.use(Vuex)
 
@@ -14,5 +15,6 @@ export default new Vuex.Store({
   actions,
   modules,
   plugins,
+  getters,
   strict: process.env.NODE_ENV !== 'production',
 })
