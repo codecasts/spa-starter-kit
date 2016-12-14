5ef5dc4ca45574f3fb1c04e94da3f87d22f098cd
Fabio Vedovelli <vedovelli@gmail.com>
2016-12-14 21:06:19 -0200

    ElementUI - navbar
    This is the first component to receive Element UI.
    
    Also some changes to work with the new structure

--- client/src/components/root/navbar.vue
+++ client/src/components/root/navbar.vue
@@ -1,78 +1,79 @@
 
 <script>
-  import { mapActions, mapGetters } from 'vuex'
-  import { version } from '../../config'
+  import { mapActions, mapState } from 'vuex'
   import CcSpinner from '../general/spinner'
+  import { version } from '../../config'
 
   export default {
     components: {
       CcSpinner,
     },
     computed: {
-      ...mapGetters(['currentUser', 'isLogged']),
+      ...mapState({
+        user: state => state.Auth.user,
+      }),
       version() {
         return version
       },
     },
-    watch: {
-      isLogged(value) {
-        if (value === false) {
-          this.$router.push({ name: 'auth.singin' })
+    methods: {
+      ...mapActions(['setToken', 'setUser']),
+      logout() {
+        this.setToken('')
+        this.setUser({})
+        this.$router.push({ name: 'login.index' })
+      },
+      /* eslint-disable no-undef */
+      navigate(name) {
+        switch (name) {
+          case 'codecasts':
+            window.location = 'https://codecasts.com.br/'
+            break;
+          case 'logout':
+            this.logout()
+            break;
+          default:
+            this.$router.push({ name })
+            break;
         }
       },
-    },
-    methods: {
-      ...mapActions(['logout']),
     },
   }
 </script>
 
 <template>
   <div>
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header spacer">
-          <a class="navbar-brand" href="http://www.codecasts.com.br/">
-            Codecasts.com.br
-          </a><br>
-          <small class="version">version {{ version }}</small>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <router-link tag="li" :to="{ name: 'dashboard.index' }" exact>
-              <a>Dashboard</a>
-            </router-link>
-            <router-link tag="li" :to="{ name: 'categories.index', query: { page: 1 } }">
-              <a>Categories</a>
-            </router-link>
-            <router-link tag="li" :to="{ name: 'products.index', query: { page: 1 } }">
-              <a>Products</a>
-            </router-link>
-          </ul>
-          <div class="nav navbar-form navbar-right">
-            <cc-spinner></cc-spinner>
-            <span class="username">{{ currentUser.name }}</span>
-            <button class="btn btn-default btn-sm" @click="logout">Logout</button>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <el-menu theme="dark" default-active="1" class="cc-navigation"
+      mode="horizontal" @select="navigate">
+      <el-menu-item index="codecasts" class="brand">Codecasts.com.br</el-menu-item>
+      <el-menu-item index="dashboard.index">Dashboard</el-menu-item>
+      <el-submenu index="menu-modules">
+        <template slot="title">Modules</template>
+        <el-menu-item index="categories.index">Categories</el-menu-item>
+        <el-menu-item index="products.index">Products</el-menu-item>
+      </el-submenu>
+      <el-submenu index="menu-user" class="logout-button">
+        <template slot="title">{{ user.name }}</template>
+        <el-menu-item index="logout">Logout</el-menu-item>
+      </el-submenu>
+    </el-menu>
+    <small class="version">version {{ version }}</small>
   </div>
 </template>
 
 <style scoped>
-  .spacer {
-    margin-left: 120px;
+  .cc-navigation {
+    padding-left: 115px;
   }
-  .username {
-    padding: 6px 20px;
-    border-radius: 20px;
-    background-color: #ddd;
-    display: inline-block;
+  .brand {
+    font-size: 1.2em;
+  }
+  .logout-button {
+    float: right;
   }
   .version {
     position: absolute;
     right: 15px;
-    top: 58px;
+    top: 65px;
   }
 </style>
