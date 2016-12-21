b5deface7210c15796f9f9dfa0e822c350838176
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-12-21 19:36:01 -0200

    generate stats.js file to webpack analyzer

--- client/build/build.js
+++ client/build/build.js
@@ -4,21 +4,24 @@ require('shelljs/global')
 env.NODE_ENV = 'production'
 
 var path = require('path')
+var fs = require('fs')
 var config = require('../config')
 var ora = require('ora')
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
+var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
+var statsFile = path.join(config.build.assetsRoot, '/stats.json')
+var spinner = ora('building for production...')
 
 console.log(
   '  Tip:\n' +
   '  Built files are meant to be served over an HTTP server.\n' +
-  '  Opening index.html over file:// won\'t work.\n'
+  '  Opening index.html over file:// won\'t work.\n' +
+  '  Build path: ' + config.build.assetsRoot + '\n'
 )
 
-var spinner = ora('building for production...')
 spinner.start()
 
-var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
 cp('-R', 'static/*', assetsPath)
@@ -26,11 +29,14 @@ cp('-R', 'static/*', assetsPath)
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
   if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n')
+
+  fs.writeFile(statsFile, JSON.stringify(stats.toJson()), () => {
+    process.stdout.write(stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+    }) + '\n')
+  })
 })
