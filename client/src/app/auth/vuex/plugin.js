4319343f227ac0c35f861f2477ac487a1ee17b27
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-12-21 13:04:23 -0200

    add config key for auth token

--- client/src/app/auth/vuex/plugin.js
+++ client/src/app/auth/vuex/plugin.js
@@ -1,22 +1,15 @@
-import { localStorageSetItem } from 'utils/local'
+import localforage from 'localforage'
+import { userTokenStorageKey } from 'src/config'
 import * as TYPES from './mutations-types'
 
 const subscribe = (store) => {
   store.subscribe((mutation, { Auth }) => {
-    if (TYPES.SET_USER === mutation.type) {
-      /**
-       * Sets the user to the local storage
-       * for auto-login purpose
-       */
-      localStorageSetItem('user', { user: Auth.user })
-    }
-
     if (TYPES.SET_TOKEN === mutation.type) {
       /**
        * Sets the token to the local storage
        * for auto-login purpose
        */
-      localStorageSetItem('token', { token: Auth.token })
+      localforage.setItem(userTokenStorageKey, Auth.token)
     }
   })
 }
