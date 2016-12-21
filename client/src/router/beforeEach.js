b815ca5d76e5684f7b8f425be5a7e2ba156a955e
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-12-21 13:05:31 -0200

    use checkUserToken in before route

--- client/src/router/beforeEach.js
+++ client/src/router/beforeEach.js
@@ -1,8 +1,6 @@
 import store from '../store'
-import { isEmpty } from 'lodash'
-import { localStorageGetItem } from '../utils/local'
 
-const needAuth = (auth, token) => auth !== undefined && auth && isEmpty(token)
+const needAuth = auth => auth === true
 
 const beforeEach = (to, from, next) => {
   /**
@@ -11,47 +9,29 @@ const beforeEach = (to, from, next) => {
   */
   store.dispatch('resetMessages')
 
-  let token = store.state.Auth.token
   const auth = to.meta.requiresAuth
-  /**
-  * If there's no token stored in the state
-  * then check localStorage:
-  */
-  if (isEmpty(token)) {
-    const localStoredToken = localStorageGetItem('token')
-    const localStoredUser = localStorageGetItem('user')
-
-    /**
-    * Do we have token and user local stored?
-    * If so then use it!
-    */
-    if (localStoredToken !== undefined &&
-        localStoredToken !== null &&
-        localStoredUser !== undefined &&
-        localStoredUser !== null
-      ) {
-      token = localStoredToken.token
-      store.dispatch('setToken', token)
-      store.dispatch('setUser', localStoredUser.user)
-    }
-  }
 
   /**
   * If route doesn't require authentication
-  * OR we have a token then let the route
   * be normally accessed.
   */
-  if (!needAuth(auth, token)) {
+  if (!needAuth(auth)) {
     next()
   }
 
   /**
   * Otherwise  if authentication is required
-  * AND the token is empty, then redirect to
   * login.
   */
-  if (needAuth(auth, token)) {
-    next({ name: 'auth.singin' })
+  if (needAuth(auth)) {
+    store.dispatch('checkUserToken')
+      .then(() => {
+        next()
+      })
+      .catch((err) => {
+        console.log({ err });
+        next({ name: 'auth.singin' })
+      })
   }
 }
 
