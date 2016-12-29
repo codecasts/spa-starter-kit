b870c51a05a8c6b67b5448dcc7d030ed6f6814a8
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-12-29 12:51:08 -0200

    update parameters test

--- webservice/tests/Support/ParametersTest.php
+++ webservice/tests/Support/ParametersTest.php
@@ -12,30 +12,30 @@ class ParametersTest extends TestCase
     public function can_get_order()
     {
         $parameters = $this->getQueryParameterInstance([
-            'order' => 'desc',
+            'order' => 'some_order',
         ]);
 
-        $this->assertEquals('desc', $parameters->order());
+        $this->assertEquals('some_order', $parameters->order());
     }
 
     /** @test */
     public function can_get_sort()
     {
         $parameters = $this->getQueryParameterInstance([
-            'sort' => 'id',
+            'sort' => 'column_name',
         ]);
 
-        $this->assertEquals('id', $parameters->sort());
+        $this->assertEquals('column_name', $parameters->sort());
     }
 
     /** @test */
     public function can_get_limit()
     {
         $parameters = $this->getQueryParameterInstance([
-            'limit' => 10,
+            'limit' => 99,
         ]);
 
-        $this->assertEquals(10, $parameters->limit());
+        $this->assertEquals(99, $parameters->limit());
     }
 
     /** @test */
@@ -49,11 +49,11 @@ class ParametersTest extends TestCase
     }
 
     /**
-     * Get a fresh instance of QueryParameter class.
+     * Get a fresh instance of the query parameters class.
      *
      * @param  array  $parameters
      *
-     * @return \App\Support\QueryParameter
+     * @return \App\Support\Parameters
      */
     private function getQueryParameterInstance($parameters = [])
     {
