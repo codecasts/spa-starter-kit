26169c477ea2f5d651576e32de55a4303a9f3834
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-12-30 10:44:43 -0200

    added option to choose the data serializer

--- webservice/config/api.php
+++ webservice/config/api.php
@@ -4,6 +4,24 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Data Serializer
+    |--------------------------------------------------------------------------
+    |
+    | Here you may choose the default data serializer to be used as the API
+    | output format, serializers let you switch between various output formats
+    | with minimal effect on your transformers.
+    |
+    | Supported: DataArraySerializer, ArraySerializer and JsonApiSerializer
+    |
+    | You can also create your own serializer, see more at the link below:
+    | http://fractal.thephpleague.com/serializers
+    |
+    */
+
+    'serializer' => League\Fractal\Serializer\DataArraySerializer::class,
+
+    /*
+    |--------------------------------------------------------------------------
     | Default transformers
     |--------------------------------------------------------------------------
     |
