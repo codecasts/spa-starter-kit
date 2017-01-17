4ef42b0ffb727a27158ffb4b35b529d491a1c528
angelorubin <angelorubin@gmail.com>
2017-01-17 13:59:52 -0200

    Correção ortografica das palavras signin/signup

--- client/src/app/auth/routes/components.js
+++ client/src/app/auth/routes/components.js
@@ -4,5 +4,5 @@
 */
 /* eslint-disable global-require */
 export const Main = r => require.ensure([], () => r(require('../components/main')), 'auth-bundle')
-export const Singin = r => require.ensure([], () => r(require('../components/forms/singin')), 'auth-bundle')
-export const Singup = r => require.ensure([], () => r(require('../components/forms/singup')), 'auth-bundle')
+export const Signin = r => require.ensure([], () => r(require('../components/forms/signin')), 'auth-bundle')
+export const Signup = r => require.ensure([], () => r(require('../components/forms/signup')), 'auth-bundle')
